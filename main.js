8178c433ad55a5cdabf760c3d19368ec4747afb1
nick v <nvalente.electrics@gmail.com>
2023-04-11 07:48:02 +0200

    added my Kata, tests and code - 1 test fails

--- main.js
+++ main.js
@@ -1,5 +1,48 @@
+
+// 👉 Write your kata here!
+
 /* 
-👉 Write your kata here!
+
+Look outside! ...it's been snowing for three days straight. You know what that means... It's time to go powder skiing! 
+But first, you need to grab the correct bag with your equipment in. Write a script to search through all five of your 'gearBags' to find your 'powder skis' and return the correct bag 
+
+Note: you must use the `find` method to locate your bag...! 
+
 */
 
-//👉 Write the function your CodeWarriors will start with below here:
+
+// 👉 Write the function your CodeWarriors will start with below here:
+
+const gearBags = [
+    { name: "gearBag1", contents: ["climbing shoes", "harness", "rock climbing helmet", "twin ropes"] },
+    { name: "gearBag2", contents: ["ice axes", "crampons", "ice harness", "ice screws", "warm gloves", "helmet"] },
+    { name: "gearBag3", contents: ["caving suit", "knee pads", "head torch", "dry sack"] },
+    { name: "gearBag4", contents: ["ski touring skis", "lightweight harness", "alloy crampons", "alloy ice axe", "avalanche tranceiver", "glacier rope", "sun glasses"] },
+    { name: "gearBag5", contents: ["avalanche backpack", "ski helmet", "ski goggles", "powder skis", "avalanche transceiver"] }
+  ];
+  
+
+export function findPowderSkis(gearBags) {
+
+  // Write your code here...
+ 
+
+    let result = gearBags.find(gearBag => gearBag.contents.includes("powder skis"));
+    return result;
+
+
+
+}
+
+
+// 👉 Write your code below here:
+
+
+
+
+
+
+
+
+  
+
--- main.test.js
+++ main.test.js
@@ -1,1 +1,75 @@
 //👉 Write your tests below here:
+
+import { findPowderSkis } from "./main.js";
+import { test, expect } from "@jest/globals";
+
+// const { findPowderSkis } = require('./main.js');
+
+
+
+
+// Write a test to check that the findPowderSkis function returns the correct gearBag with the powder skis (number 5)
+
+    test ("When given the list of gearBags, the findPowderSkis function returns the correct gearBag with the powder skis", () => {
+        const gearBags = [
+            { name: "gearBag1", contents: ["climbing shoes", "harness", "rock climbing helmet", "twin ropes"] },
+            { name: "gearBag2", contents: ["ice axes", "crampons", "ice harness", "ice screws", "warm gloves", "helmet"] },
+            { name: "gearBag3", contents: ["caving suit", "knee pads", "head torch", "dry sack"] },
+            { name: "gearBag4", contents: ["ski touring skis", "lightweight harness", "alloy crampons", "alloy ice axe", "avalanche tranceiver", "glacier rope", "sun glasses"] },
+            { name: "gearBag5", contents: ["avalanche backpack", "ski helmet", "ski goggles", "powder skis", "avalanche transceiver"] }
+          ];
+        const actual = findPowderSkis(gearBags);
+        const gearBag5 = { name: "gearBag5", contents: ["avalanche backpack", "ski helmet", "ski goggles", "powder skis", "avalanche transceiver"] };
+        const expected = gearBag5;
+        
+        expect(actual).toStrictEqual(expected);
+        });
+
+//=======================================
+
+
+// Write a test that checks if the correct gearBag is returned by listing its contents 
+
+test('findPowderSkis should return the gear bag containing "powder skis"', () => {
+    const gearBags = [
+        { name: "gearBag1", contents: ["climbing shoes", "harness", "rock climbing helmet", "twin ropes"] },
+        { name: "gearBag2", contents: ["ice axes", "crampons", "ice harness", "ice screws", "warm gloves", "helmet"] },
+        { name: "gearBag3", contents: ["caving suit", "knee pads", "head torch", "dry sack"] },
+        { name: "gearBag4", contents: ["ski touring skis", "lightweight harness", "alloy crampons", "alloy ice axe", "avalanche tranceiver", "glacier rope", "sun glasses"] },
+        { name: "gearBag5", contents: ["avalanche backpack", "ski helmet", "ski goggles", "powder skis", "avalanche transceiver"] }
+      ];
+      
+    const result = findPowderSkis(gearBags);
+    expect(result).toEqual({"contents": ["avalanche backpack", "ski helmet", "ski goggles", "powder skis", "avalanche transceiver"], "name": "gearBag5"});
+  });
+
+//=======================================
+  
+
+// Write a code to make sure "find" method is used in the findPowderSkis function
+
+    test("findPowderSkis function should use the find method", () => {
+        const gearBags = [
+            { name: "gearBag1", contents: ["climbing shoes", "harness", "rock climbing helmet", "twin ropes"] },
+            { name: "gearBag2", contents: ["ice axes", "crampons", "ice harness", "ice screws", "warm gloves", "helmet"] },
+            { name: "gearBag3", contents: ["caving suit", "knee pads", "head torch", "dry sack"] },
+            { name: "gearBag4", contents: ["ski touring skis", "lightweight harness", "alloy crampons", "alloy ice axe", "avalanche tranceiver", "glacier rope", "sun glasses"] },
+            { name: "gearBag5", contents: ["avalanche backpack", "ski helmet", "ski goggles", "powder skis", "avalanche transceiver"] }
+          ];         
+      
+        const findSpy = jest.spyOn(gearBags, "find");
+      
+        findPowderSkis(gearBags);
+      
+        expect(findSpy).toHaveBeenCalled();
+      });
+      
+
+//=======================================
+  
+// OTHER TESTS CONSIDERED //
+
+// a test to ensure one of the arrays contains 'power skis'
+// a test to return "null" if none of the arrays contain 'powder skis'
+// a test to ensure multiple arrays don't contain 'powder skis'
+// a test to ensure the arrays contain text and not empty strings
